ba21de1f69cbf21d448657baf8af7971c28cee5a
diemp94 <diemp94@hotmail.com>
2018-04-08 21:44:16 -0500

    Minuto 19
    Se adelanto lo visto hasta el minuto 19 del video

--- app/src/main/java/com/proyecto/diego/httprequest/MainActivity.java
+++ app/src/main/java/com/proyecto/diego/httprequest/MainActivity.java
@@ -7,7 +7,10 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +20,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://samples.openweathermap.org/data/2.5/").build();
+                .baseUrl("http://samples.openweathermap.org/data/2.5/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
         WheatherService service = retrofit.create(WheatherService.class);
 
 
         Call<City> cityCall = service.getCity("524901","b1b15e88fa797225412429c1c50c122a1");
 
+        cityCall.enqueue(new Callback<City>() {
+            @Override
+            public void onResponse(Call<City> call, Response<City> response) {
+                City city = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<City> call, Throwable t) {
+                Toast.makeText(MainActivity.this,"Error",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
